5b1d86f679b652cd180b675a9964bc24a8460009
prirya <prirya2@gmail.com>
2021-01-21 03:18:51 +0700

    Add project files.

---
+++ SQLiteApp/DAtaAccess.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteApp
+{
+    class DAtaAccess
+    {
+        public const string dbpath = "Customer.db";  //เป็นการประการไปเลยว่า dbpath คือการใช้ไฟล์ที่ชื่อ Customer.db เพื่อลดขั้นตอนการเขียนชื่อซ้ำไปซ้ำมา และป้องกันการเขียนชื่อไฟล์ผิดในบรรทัดอื่นๆ
+        private static object createTable;
+
+        public static void InitializeDatabase() //สร้างตาราง
+        {
+            //INTEGER   TINYINT     = 1 byte = 127
+            //          SMALLINT    = 2 byte = 32767
+            //          MEDIUMINT   = 3 byte = 8388607
+            //          INT(11)     = 4 byte = 2147483647
+            //          BIGINT      = 8 byte = 9223372036854775807
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS CustomerTable (uid INTEGER PRIMARY KEY, " +
+                    "first_Name NVARCHAR(50) NULL, " +
+                    "last_Name NVARCHAR(50) NULL, " +
+                    "email NVARCHAR(50) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+
+            }
+        }
+        public static void AddData(string firstName, string lastName,string email)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO CustomerTable VALUES (NULL, @first_Name,@last_Name,@email);";
+                insertCommand.Parameters.AddWithValue("@first_Name", firstName);
+                insertCommand.Parameters.AddWithValue("@last_Name", lastName);
+                insertCommand.Parameters.AddWithValue("@email", email);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<String> GetData() //คือการแสดงผลข้อมูลที่อยู่ใน dataBase ออกมาทั้งหมด
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT first_Name,last_Name,email from CustomerTable", db); //หลัง SELECT คือใส่ชื่อ field หากมีหลาย field ข้องใส่ (,) เอาไว้ด้วย แต่ถ้าอยากได้ทั้งหมดเลยก็ใส่ (*)
+               
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                
+                while (query.Read())
+                {
+                    entries.Add($"{query.GetString(0)} {query.GetString(1)} ({query.GetString(2)})"); //มันใช้งานเหมือน ArrayList คือกำหนดให้ส่งค่ากลับออกมาเป็นแต่ละ column (field) โดยเรียงตามที่เราเรียง Opject [0][1][2]
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+    }
+}
---
+++ SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLiteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DAtaAccess.InitializeDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textResult = ""; //ข้อความว่างสำหรับเก็บ Entry อื่นๆ
+            List<string> getData = DAtaAccess.GetData(); //เอาข้อมูลจาก database มาเก็บเป็นตัวแปล getdata
+            for (int i = 0; i < getData.Count; i++) //วนซ้ำตามเงื่อนไข
+            {
+                textResult += getData[i] + "\n"; //ขึ้นบรรทัดใหม่ // += คือ ต่อเติมค่าที่อยู่ข้างหน้าเข้าไปหาค่าที่อยู่ข้างหลัง ยกตัวอย่าง A += B ก็จะได้ AB
+            }
+            MessageBox.Show(textResult);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DAtaAccess.AddData(firstNameBox.Text, lastNameBox.Text, emailBox.Text);
+            firstNameBox.Text = lastNameBox.Text = emailBox.Text = "";
+        }
+    }
+}
